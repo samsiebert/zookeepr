d76b749e332b7dcea5f518a6d54ea6659f5f686b
samsiebert <sammsiebert@gmail.com>
2022-07-11 22:25:28 -0500

    corrected type in path animal to animals

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const {animals} = require('./data/animals');
 const express = require('express');
+const { animals } = require('./data/animals');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
-
     let filteredResults = animalsArray;
+
     if (query.personalityTraits) {
         //Save personalityTraits as dedidcated array
         //If personalitytraits is a string, place it into a new array and save
@@ -22,11 +23,11 @@ function filterByQuery(query, animalsArray) {
             animal => animal.personalityTraits.indexOf(trait) !== -1
         );
     });
-    }
+}
 if (query.diet) {
     filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
 }
-if (query.sepcies) {
+if (query.species) {
     filteredResults = filteredResults.filter(animal => animal.species === query.species);    }
 if (query.name) {
     filteredResults = filteredResults.filter(animal => animal.name === query.name);
@@ -35,12 +36,24 @@ if (query.name) {
 return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
